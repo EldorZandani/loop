aabc9c556bb6b166a1784c44d5f07c6116b5d3fb
EldorZandani <fox18966@gmail.com>
2022-02-06 16:57:17 +0200

    how to creat loop

---
+++ loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = int.Parse(Console.ReadLine());
+
+            for (i = 0; i > 3; i++)
+            {
+                Console.WriteLine(i * 5);
+            }
+            if (i >= 3)
+                Console.WriteLine("pass");
+            else
+                Console.WriteLine("fail");
+
+        }
+    }
+}
